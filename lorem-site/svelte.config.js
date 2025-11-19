88834ae3ec1a9ea147edcbfb93a13c72444872a1
Mistz <mathias.gagnepain@algosup.com>
2025-11-19 12:23:31 +0100

    Switch Svelte adapter to adapter-node
    Changed SvelteKit configuration to use '@sveltejs/adapter-node' instead of '@sveltejs/adapter-auto' for explicit Node.js deployment.

--- lorem-site/svelte.config.js
+++ lorem-site/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,8 +8,6 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto will automatically use adapter-node when building in Docker
-		// or you can explicitly use adapter-node by installing @sveltejs/adapter-node
 		adapter: adapter()
 	}
 };
